fcee279539a5a1d806f8de850399fce0a053e5d1
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 15:40:11 +0200

    Bekér két számot és kiírja a négyzetüket

---
+++ 019. ketszamnegyzete/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _019.ketszamnegyzete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot és kiírom a négyzetét!");
+            double szam1 = Convert.ToDouble(Console.ReadLine());
+            double negyzet = szam1 * szam1;
+            Console.WriteLine("A szám négyzete: " +negyzet + "." + "\n");
+
+            Console.WriteLine("Kérek még egy számot és kiírom ennek is a négyzetét!");
+            double szam2 = Convert.ToDouble(Console.ReadLine());
+            double negyzet2 = szam2 * szam2;
+            Console.WriteLine("A szám négyzete: " +negyzet2 + "." + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
